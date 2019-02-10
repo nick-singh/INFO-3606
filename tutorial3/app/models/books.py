54c0ac5d53c7ef0fd52a1878f33dc58832aa91f0
nicholas <nicholas.chamansingh@gmail.com>
2019-02-10 15:23:07 -0400

    initial tutorial 3

---
+++ tutorial3/app/models/books.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, unique=True, nullable=False, primary_key=True)
+    title = db.Column(db.String(80))
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+        
+    def toJSON(self):
+        return {
+            "id":self.id,
+            "title":self.title
+        }
